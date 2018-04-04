12ef4ea720482bc8294682b0638894b985c6267f
Hunter Lang <zenelk@gmail.com>
2018-04-03 22:21:28 -0600

    Started the done command.

--- index.js
+++ index.js
@@ -4,12 +4,17 @@ const Telegraf = require('telegraf')
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
+function handleDone (ctx) {
+  console.log(ctx.message.text)
+}
+
 function setupBot () {
   bot.start((ctx) => ctx.reply('I\'m started'))
   bot.help((ctx) => ctx.reply('There\'s no help for you.'))
   bot.settings((ctx) => ctx.reply('No settings.'))
 
   bot.command('/test', (ctx) => ctx.reply('I am functioning normally.'))
+  bot.command('/done', handleDone)
 
   bot.startPolling()
 }
