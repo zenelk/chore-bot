163646ba203eca5f650148fffd9b4d54230a0fab
Hunter Lang <zenelk@gmail.com>
2018-04-03 20:40:52 -0600

    More grasping at straws.

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-require('dotenv').config()
-
-// const Telegraf = require('telegraf')
-var pg = require('pg')
 var express = require('express')
 var app = express()
+var pg = require('pg')
 
-process.title = 'chore-bot'
+app.set('port', (process.env.PORT || 5000))
+
+app.use(express.static(__dirname + '/public'))
+
+// views is directory for all template files
+app.set('views', __dirname + '/views')
+app.set('view engine', 'ejs')
+
+app.get('/', function (request, response) {
+  response.render('pages/index')
+})
 
 app.get('/db', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function (err, client, done) {
@@ -20,8 +27,38 @@ app.get('/db', function (request, response) {
   })
 })
 
-// const bot = new Telegraf(process.env.BOT_TOKEN)
-// bot.command('/test', (ctx) => ctx.reply('I am functioning normally.'))
-// bot.command('/addChat')
+app.listen(app.get('port'), function () {
+  console.log('Node app is running on port', app.get('port'))
+})
 
-// bot.startPolling()
+// require('dotenv').config()
+
+// // const Telegraf = require('telegraf')
+// var pg = require('pg')
+// var express = require('express')
+// var app = express()
+
+// process.title = 'chore-bot'
+
+// app.get('/db', function (request, response) {
+//   pg.connect(process.env.DATABASE_URL, function (err, client, done) {
+//     client.query('SELECT * FROM test_table', function (err, result) {
+//       done()
+//       if (err) {
+//         console.error(err); response.send('Error ' + err)
+//       } else {
+//         response.render('pages/db', {results: result.rows})
+//       }
+//     })
+//   })
+// })
+
+// app.listen(app.get('port'), function() {
+//   console.log('Node app is running on port', app.get('port'));
+// });
+
+// // const bot = new Telegraf(process.env.BOT_TOKEN)
+// // bot.command('/test', (ctx) => ctx.reply('I am functioning normally.'))
+// // bot.command('/addChat')
+
+// // bot.startPolling()
